16ae9da2fbcb7ddb7e2557c1606fcccd1deb66b8
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-15 15:57:53 +0300

    Исправление ошибок

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1416/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task14.task1416;
+
+/* 
+Исправление ошибок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Swimmable animal = new Orca();
+        animal.swim();
+        animal = new Whale();
+        animal.swim();
+        animal = new Otter();
+        animal.swim();
+    }
+
+    public static void test(Swimmable animal) {
+        animal.swim();
+    }
+
+    interface Walkable {
+        void walk();
+    }
+
+    interface Swimmable {
+        void swim();
+    }
+
+    static abstract class OceanAnimal implements Swimmable {
+        public void swim() {
+            OceanAnimal currentAnimal = (OceanAnimal) getCurrentAnimal();
+            currentAnimal.displaySwim();
+        }
+
+        private void displaySwim() {
+            System.out.println(getCurrentAnimal().getClass().getSimpleName() + " is swimming");
+        }
+
+        abstract Swimmable getCurrentAnimal();
+    }
+
+    static class Orca extends OceanAnimal {
+        @Override
+        Swimmable getCurrentAnimal() {
+            return null;
+        }
+    }
+
+    static class Whale extends OceanAnimal {
+        @Override
+        Swimmable getCurrentAnimal() {
+            return null;
+        }
+    }
+
+    static class Otter implements Walkable, Swimmable {
+        @Override
+        public void walk() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+}
