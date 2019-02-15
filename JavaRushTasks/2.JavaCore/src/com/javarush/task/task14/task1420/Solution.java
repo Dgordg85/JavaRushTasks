a6f513e1eb561052abbe2d6c0a40a47c4e61906a
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-15 17:35:25 +0300

    Недоделана

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task14.task1420;
+
+/* 
+НОД
+*/
+
+import sun.font.FontRunIterator;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int FirstNumber = Integer.parseInt(sc.nextLine());
+            int SecondNumber = Integer.parseInt(sc.nextLine());
+            if (FirstNumber < 1 || SecondNumber < 1) {
+                throw new ArithmeticException();
+            }
+
+            for (int i = 0; i < ; i++) {
+                
+            }
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
