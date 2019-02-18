231ef84def5897d8c2604cb8ed93b9840b56090e
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-18 11:28:03 +0300

    НОД

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -4,26 +4,69 @@ package com.javarush.task.task14.task1420;
 НОД
 */
 
-import sun.font.FontRunIterator;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+
+    private static List<Integer> list = genListSimpleNumber();
+
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int nod = 1;
 
-        try {
-            int FirstNumber = Integer.parseInt(sc.nextLine());
-            int SecondNumber = Integer.parseInt(sc.nextLine());
-            if (FirstNumber < 1 || SecondNumber < 1) {
-                throw new ArithmeticException();
-            }
 
-            for (int i = 0; i < ; i++) {
-                
+        int firstNumber = Integer.parseInt(sc.nextLine());
+        int secondNumber = Integer.parseInt(sc.nextLine());
+
+        List<Integer> firstNumberList = getNOD(firstNumber);
+        List<Integer> secondNumberList = getNOD(secondNumber);
+
+        if (firstNumber < 1 || secondNumber < 1) throw new NumberFormatException();
+
+        Iterator<Integer> iterator = firstNumberList.iterator();
+
+        while (iterator.hasNext()){
+            Integer num = iterator.next();
+            if (secondNumberList.contains(num)){
+                nod *= num;
+                iterator.remove();
+                secondNumberList.remove(num);
             }
-        } catch (Exception e){
-            System.out.println(e);
         }
+        System.out.println(nod);
+
+
     }
+
+    //Получаем все множители числа
+    private static List<Integer> getNOD(Integer num){
+        List<Integer> numberList = new ArrayList<>();
+        while (num > 1){
+            for (int i = 0; i < list.size() ; i++) {
+                int currentNum = list.get(i);
+                if (num % currentNum == 0) {
+                    numberList.add(currentNum);
+                    num = num / currentNum;
+                }
+            }
+        }
+        Collections.sort(numberList);
+        return numberList;
+    }
+
+    //Генерируем лист простых чисел
+    private static List<Integer> genListSimpleNumber(){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i < 10000; i++) {
+            Boolean isNumberSimple = true;
+            for (int j = i - 1; j > 1; j--) {
+                if (i % j == 0) isNumberSimple = false;
+            }
+            if (isNumberSimple == true) list.add(i);
+        }
+        return list;
+    }
+
+
 }
