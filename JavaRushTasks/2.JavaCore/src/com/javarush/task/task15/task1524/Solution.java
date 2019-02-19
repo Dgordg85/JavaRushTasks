5c1d50a6c0161bc368fab83ffed81f496086f684
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-19 17:33:39 +0300

    Порядок загрузкипеременных

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1524/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task15.task1524;
+
+/* 
+Порядок загрузки переменных
+*/
+
+public class Solution {
+    static {
+        init();
+    }
+
+    static {
+        System.out.println("Static block");
+    }
+
+
+
+
+
+
+
+    public Solution() {
+        System.out.println("Solution constructor");
+        printAllFields(this);
+    }
+
+    public static void init() {
+        System.out.println("static void init()");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("public static void main");
+        Solution s = new Solution();
+    }
+
+    {
+        System.out.println("Non-static block");
+        printAllFields(this);
+    }
+
+    public static void printAllFields(Solution obj) {
+        System.out.println("static void printAllFields");
+        System.out.println(obj.i);
+        System.out.println(obj.name);
+
+    }
+
+    public int i = 6;
+
+    public String name = "First name";
+}
