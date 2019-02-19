d2b96847f49d8b72dfbcaf4cce70afcdc3d11c82
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-19 17:23:00 +0300

    Перегрузка конструкторов

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution(){
+
+    }
+    private Solution(int i){
+
+    }
+    protected Solution(Integer i){
+
+    }
+    Solution(String s){
+
+    }
+}
+
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    SubSolution(String s) {
+        super(s);
+    }
+}
