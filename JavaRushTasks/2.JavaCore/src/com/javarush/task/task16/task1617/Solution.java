a101457cfe8fde456df242b2ba1532053685e299
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-22 16:06:31 +0300

    Недоделана

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+import java.util.Date;
+
+public class Solution {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            while(!isInterrupted()){
+                try {
+                    if (numSeconds == 0) System.out.print(" Марш!");
+                    System.out.print(" " + numSeconds);
+                    sleep(1000);
+                    numSeconds--;
+                } catch (InterruptedException e) {
+                    System.out.print(" Прервано!");
+                }
+            }
+        }
+    }
+}
