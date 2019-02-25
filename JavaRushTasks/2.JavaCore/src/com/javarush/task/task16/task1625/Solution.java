7592e87368704d9a150fc69065dd0be00eb5c1a9
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-25 14:23:32 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1625/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1625/Solution.java
@@ -11,6 +11,7 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         t1.start();
         t2.start();
+        t1.interrupt();
     }
 
     public static class T1 extends Thread {
