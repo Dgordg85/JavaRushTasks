91b60d615a0f79642f09a7ec72829501f86d1c00
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-26 16:39:01 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -5,10 +5,110 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Solution {
     public static List<Thread> threads = new ArrayList<>(5);
 
-    public static void main(String[] args) {
+    static {
+        threads.add(new Infinity());
+        threads.add(new Except());
+        threads.add(new SayUra());
+        threads.add(new UseMessage());
+        threads.add(new ReadConsole());
     }
+
+    public static void main(String[] args) {
+        Thread t = threads.get(1);
+        t.start();
+        t.interrupt();
+
+
+
+       /* UseMessage t = (UseMessage) threads.get(3);
+
+        t.start();
+        System.out.println(t.isAlive());
+        t.showWarning();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(t.isAlive()); */
+
+    }
+
+    public static class Infinity extends Thread{
+        @Override
+        public void run() {
+            while (true){
+
+            }
+        }
+    }
+
+    public static class Except extends Thread{
+        @Override
+        public void run() {
+            try {
+                while(!isInterrupted()){
+                    Thread.sleep(500);
+                }
+            }catch (InterruptedException e){
+                System.out.println("InterruptedException");
+            }
+        }
+    }
+
+    public static class SayUra extends Thread{
+        @Override
+        public void run() {
+            while (!isInterrupted()){
+                System.out.println("Ура");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+    public static class UseMessage extends Thread implements Message{
+        @Override
+        public void run() {
+            while(!isInterrupted()){
+            }
+        }
+
+        @Override
+        public void showWarning() {
+            interrupt();
+        }
+    }
+
+
+    public static class ReadConsole extends Thread{
+        private String str;
+        private int sum;
+
+        @Override
+        public void run() {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            while (!isInterrupted()){
+                try{
+                    if (reader.ready()){
+                        str = reader.readLine();
+                        if (!str.equals("N")){
+                            sum += Integer.parseInt(str);
+                        } else {
+                            System.out.println(sum);
+                            break;
+                        }
+                    }
+                } catch (Exception e){}
+            }
+        }
+    }
+
+
 }
