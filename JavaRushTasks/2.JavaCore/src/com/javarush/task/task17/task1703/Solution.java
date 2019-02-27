39a708c43c1bd0cf977a9290ad0f024476152e24
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-27 14:48:01 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1703/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1703/Solution.java
@@ -13,20 +13,29 @@ public class Solution {
 
     }
 
-    public static class Note {
+    public static class Note extends Thread {
 
-        public final List<String> notes = new ArrayList<String>();
+        public final List<String> notes = new ArrayList<>();
 
         public void addNote(int index, String note) {
             System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
-            notes.add(index, note);
+            synchronized (notes){
+                notes.add(index, note);
+            }
             System.out.println("Уже добавлена заметка [" + note + "]");
         }
 
         public void removeNote(int index) {
             System.out.println("Сейчас будет удалена заметка с позиции " + index);
-            String note = notes.remove(index);
+            String note;
+            synchronized (notes){
+                note = notes.remove(index);
+            }
             System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
+        }
+
+        public void run(){
+
         }
     }
 
