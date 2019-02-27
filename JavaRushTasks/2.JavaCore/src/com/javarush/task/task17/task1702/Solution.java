32dd2d13a1e1162781e08d7ca0e76c55f67e0a5a
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-27 12:39:36 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
@@ -34,7 +34,7 @@ public class Solution {
     }
 
     public static void initThreads() throws InterruptedException {
-        List<Thread> threads = new ArrayList<Thread>(threadCount);
+        List<Thread> threads = new ArrayList<>(threadCount);
         for (int i = 0; i < threadCount; i++) threads.add(new SortThread());
         for (Thread thread : threads) thread.start();
         for (Thread thread : threads) thread.join();
@@ -49,6 +49,13 @@ public class Solution {
                     array[j] = k;
                 }
             }
+        }
+    }
+
+    public static class SortThread extends Thread{
+        @Override
+        public void run() {
+            sort(testArray);
         }
     }
 }
