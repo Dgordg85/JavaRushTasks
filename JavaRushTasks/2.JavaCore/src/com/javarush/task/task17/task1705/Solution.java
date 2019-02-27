b1514fe95c2a0417db0ce528f8630d47acf6091f
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-27 15:07:03 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1705/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1705/Solution.java
@@ -14,10 +14,26 @@ public class Solution {
 
     }
 
-    public static class Garden {
+    public static class Garden extends Thread {
 
-        public final List<String> fruits = new ArrayList<String>();
-        public final List<String> vegetables = new ArrayList<String>();
+        public final List<String> fruits = new ArrayList<>();
+        public final List<String> vegetables = new ArrayList<>();
+
+        public synchronized void addFruit(int index, String fruit){
+            fruits.add(index, fruit);
+        }
+
+        public synchronized void removeFruit(int index){
+            fruits.remove(index);
+        }
+
+        public synchronized void addVegetable(int index, String fruit){
+            vegetables.add(index, fruit);
+        }
+
+        public synchronized void removeVegetable(int index){
+            vegetables.remove(index);
+        }
 
     }
 }
