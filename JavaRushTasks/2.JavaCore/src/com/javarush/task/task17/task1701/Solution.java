16a2478b7e4bf28737780b35fe15ab25a77f85cc
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-27 12:12:02 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1701/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1701/Solution.java
@@ -22,13 +22,33 @@ public class Solution {
         }
 
         public static void removeNote(String threadName) {
-            String note = notes.remove(0);
+            String note;
+            try {
+                note = notes.remove(0);
+            }catch (Exception e) {
+                note = null;
+            }
             if (note == null) {
                 System.out.println("Другая нить удалила нашу заметку");
             } else if (!note.startsWith(threadName)) {
                 System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
             } else {
                 System.out.println("Нить [" + threadName + "] удалила свою заметку [" + note + "]");
+            }
+        }
+    }
+
+    public static class NoteThread extends Thread{
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                Note.addNote(getName() + "-Note" + i);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Note.removeNote(getName());
             }
         }
     }
