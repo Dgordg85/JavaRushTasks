5c6fb2bb7790fb3727f451de078163a482ce15cf
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-28 10:59:52 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1717/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1717/Solution.java
@@ -9,25 +9,12 @@ public class Solution {
     int count;
 
     public Solution append(CharSequence s) {
-        synchronized (Solution.class) {
-            if (s == null) {
-                synchronized (this) {
-                    s = "null";
-                }
-            }
+        if (s == null) s = "null";
 
-            if (s instanceof String) {
-                synchronized (this) {
-                    return this.append((String) s);
-                }
-            }
+        if (s instanceof String) return this.append((String) s);
 
-            if (s instanceof Solution) {
-                synchronized (this) {
-                    return this.appendThis((Solution) s);
-                }
-            }
-        }
+        if (s instanceof Solution) return this.appendThis((Solution) s);
+
         return this.append(s);
     }
 
@@ -45,22 +32,18 @@ public class Solution {
 
     private synchronized void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
         java.io.ObjectOutputStream.PutField fields = s.putFields();
-        synchronized (fields) {
-            fields.put("value", value);
-            fields.put("count", count);
-            fields.put("shared", false);
-        }
-        synchronized (s) {
-            s.writeFields();
-        }
+
+        fields.put("value", value);
+        fields.put("count", count);
+        fields.put("shared", false);
+
+        s.writeFields();
     }
 
     private void readObject(java.io.ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
-        synchronized (new java.io.IOException()) {
-            java.io.ObjectInputStream.GetField fields = s.readFields();
-            value = (char[]) fields.get("value", null);
-            count = fields.get("count", 0);
-        }
+        java.io.ObjectInputStream.GetField fields = s.readFields();
+        value = (char[]) fields.get("value", null);
+        count = fields.get("count", 0);
     }
 
     public static void main(String[] args) {
