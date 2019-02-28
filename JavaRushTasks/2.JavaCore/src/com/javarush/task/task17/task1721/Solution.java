1895af866488ffce2894e213248f1ade71af830e
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-02-28 15:21:52 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,13 +9,31 @@ import java.util.List;
 */
 
 public class Solution {
-    public static List<String> allLines = new ArrayList<String>();
-    public static List<String> forRemoveLines = new ArrayList<String>();
+    public static List<String> allLines = new ArrayList<>();
+    public static List<String> forRemoveLines = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CorruptedDataException {
+        readFile(allLines);
+        readFile(forRemoveLines);
+        new Solution().joinData();
+    }
+
+    private static void readFile(List<String> list){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())));
+
+            while (fileReader.ready()) list.add(fileReader.readLine());
+        } catch (IOException e){}
     }
 
     public void joinData() throws CorruptedDataException {
+        if (!allLines.containsAll(forRemoveLines)){
+            allLines.clear();
+            throw new CorruptedDataException();
+        } else {
+            forRemoveLines.forEach((String str) -> allLines.remove(str));
+        }
 
     }
 }
