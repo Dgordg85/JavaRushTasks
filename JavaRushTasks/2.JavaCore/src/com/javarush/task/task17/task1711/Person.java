7047879620558e4f002d9aa093d9872e27ff67a3
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-01 14:32:47 +0300

    CRUD 2

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1711/Person.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1711/Person.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task17.task1711;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+
+import static com.javarush.task.task17.task1711.Solution.allPeople;
 
 public class Person {
     private String name;
@@ -43,5 +48,38 @@ public class Person {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+
+    public static void createPeopleById(String name, String sex, String dateStr) throws ParseException {
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/y");
+
+        if (sex.equals("м")) {
+            allPeople.add(Person.createMale(name, date.parse(dateStr)));
+        } else {
+            allPeople.add(Person.createFemale(name, date.parse(dateStr)));
+        }
+        System.out.print(allPeople.size() - 1 + " ");
+    }
+
+    public static void updatePeopleById(String id, String name, String sex, String dateStr) throws ParseException {
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/y");
+        Person iP = allPeople.get(Integer.parseInt(id));
+        iP.setName(name);
+        iP.setSex(sex.equals("м") ? Sex.MALE : Sex.FEMALE);
+        iP.setBirthDate(date.parse(dateStr));
+    }
+
+    public static void nullPeopleById(int id){
+        Person iP = allPeople.get(id);
+        iP.setBirthDate(null);
+        iP.setName(null);
+        iP.setSex(null);
+    }
+
+    public static void getPeopleById(int id){
+        Person iP = allPeople.get(id);
+        SimpleDateFormat modifyDate = new SimpleDateFormat("dd-MMM-y", Locale.ENGLISH);
+        System.out.println(iP.getName() + " " + (iP.getSex().equals(Sex.MALE) ? "м" : "ж") + " " + modifyDate.format(iP.getBirthDate()));
     }
 }
--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task17.task1711;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,14 +11,47 @@ CRUD 2
 */
 
 public class Solution {
-    public static volatile List<Person> allPeople = new ArrayList<Person>();
+    public static volatile List<Person> allPeople = new ArrayList<>();
 
     static {
         allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+        /*try {
+            allPeople.add(Person.createMale("Миронов", (new SimpleDateFormat("d/MM/y").parse("15/04/1990"))));
+            allPeople.add(Person.createFemale("Миронова", (new SimpleDateFormat("d/MM/y").parse("25/04/1997"))));
+        } catch (ParseException e) {}*/
     }
 
-    public static void main(String[] args) {
-        //start here - начни тут
+    public static void main(String[] args) throws ParseException {
+        try {
+            switch (args[0]) {
+                case "-c":
+                    synchronized (allPeople) {
+                        int countC = 0;
+                        while (true) {
+                            Person.createPeopleById(args[1 + countC * 3], args[2 + countC * 3], args[3 + countC * 3]);
+                            countC++;
+                        }
+                    }
+                case "-u":
+                    synchronized (allPeople) {
+                        int countU = 0;
+                        while (true) {
+                            Person.updatePeopleById(args[1 + countU * 4], args[2 + countU * 4], args[3 + countU * 4], args[4 + countU * 4]);
+                            countU++;
+                        }
+                    }
+                case "-d":
+                    synchronized (allPeople) {
+                        int d = 1;
+                        while (true) Person.nullPeopleById(Integer.parseInt(args[d++]));
+                    }
+                case "-i":
+                    synchronized (allPeople) {
+                        int i = 1;
+                        while (true) Person.getPeopleById(Integer.parseInt(args[i++]));
+                    }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {}
     }
 }
