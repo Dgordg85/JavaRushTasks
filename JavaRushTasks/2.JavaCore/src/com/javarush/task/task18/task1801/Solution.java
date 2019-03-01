40d6ffa34c9a30ab6816725afb8cd78dc760fad8
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-01 16:01:41 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task18.task1801;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /* 
 Максимальный байт
@@ -8,5 +10,14 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream file = new FileInputStream(reader.readLine());
+        int max = 0;
+        int currentByte;
+        while (file.available() > 0) {
+            if ((currentByte = file.read()) > max) max = currentByte;
+        }
+        System.out.println(max);
+        file.close();
     }
 }
