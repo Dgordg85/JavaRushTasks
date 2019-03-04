c61a2c82acdec556e2f7f091096b5cdde89f9e9c
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-04 14:30:32 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task18.task1803;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Самые частые байты
@@ -8,5 +13,27 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream file = new FileInputStream(reader.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while (file.available() > 0) {
+            int currentByte = file.read();
+            if (map.containsKey(currentByte)) {
+                map.replace(currentByte, map.get(currentByte).intValue() + 1);
+            } else {
+                map.put(currentByte, 1);
+            }
+        }
+        int maxMapValue = Collections.max(map.values());
+        for (Map.Entry entry : map.entrySet()){
+            if (entry.getValue().equals(maxMapValue)) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+
+        file.close();
+        reader.close();
+
     }
 }
