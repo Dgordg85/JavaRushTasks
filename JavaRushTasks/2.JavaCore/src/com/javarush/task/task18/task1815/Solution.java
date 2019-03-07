9a63942b6a19e6b2e91df13c5f752b1cbe49e543
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-07 16:07:00 +0300

    Create Solution.java

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1815/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task18.task1815;
+
+import java.util.List;
+
+/* 
+Таблица
+*/
+
+public class Solution {
+    public class TableInterfaceWrapper implements TableInterface {
+        TableInterface component;
+
+        public TableInterfaceWrapper(TableInterface component) {
+            this.component = component;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            component.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return component.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            component.setHeaderText(newHeaderText);
+        }
+    }
+
+    public interface TableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+    }
+}
