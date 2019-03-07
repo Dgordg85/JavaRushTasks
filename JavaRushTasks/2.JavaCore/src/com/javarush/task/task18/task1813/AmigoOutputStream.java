88a83eddb695272ed059db776253c0982f523f53
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-07 12:52:02 +0300

    Update AmigoOutputStream.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -8,11 +8,43 @@ import java.io.IOException;
 AmigoOutputStream
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream {
     public static String fileName = "C:/tmp/result.txt";
+    public FileOutputStream component;
 
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
+    public AmigoOutputStream(FileOutputStream component) throws FileNotFoundException {
+        super("Java");
+        this.component = component;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        component.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        component.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        component.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        component.flush();
+        component.write("JavaRush © All rights reserved.".getBytes());
+        component.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        component.flush();
+    }
 }
