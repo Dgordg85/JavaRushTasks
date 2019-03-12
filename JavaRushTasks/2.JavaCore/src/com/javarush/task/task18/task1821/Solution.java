7ab2be99b586b85dee50c9f03fd9686943bf81dd
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-12 15:10:18 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,28 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileInputStream stream = new FileInputStream(args[0]);
+        BufferedInputStream reader = new BufferedInputStream(stream);
+
+        int[] buffer = new int[256];
+        int i;
+
+        while ((i = reader.read()) != -1){
+            buffer[i]++;
+        }
+
+        for (int j = 0; j < buffer.length; j++) {
+            if (buffer[j] != 0){
+                System.out.println((char) j + " " + buffer[j]);
+            }
+        }
+
+        reader.close();
     }
 }
