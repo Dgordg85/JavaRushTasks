a9afdbc7a03d4a48693d2109e32305401aecd410
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-12 16:49:27 +0300

    Собираем файл

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task18.task1825;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Собираем файл
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
