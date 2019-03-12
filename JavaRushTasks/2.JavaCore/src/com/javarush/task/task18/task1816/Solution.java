1c890fd8ecf4c87db53f31cca1ad74d1ed966ae6
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-12 10:50:37 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -4,14 +4,21 @@ package com.javarush.task.task18.task1816;
 Английские буквы
 */
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         FileInputStream stream = new FileInputStream(args[0]);
-        BufferedReader reader = new BufferedReader(stream, 6400);
+        BufferedInputStream reader = new BufferedInputStream(stream);
+        int i;
+        int countChar = 0;
+        while ((i = reader.read()) != -1){
+            if (i > 64 && i < 91 || i > 96 && i < 123) countChar++;
+        }
+        System.out.println(countChar);
+        reader.close();
         stream.close();
     }
 }
