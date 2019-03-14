84a30f4d0f6b140e40f033772949d9ba10153372
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-14 09:48:14 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.task18.task1827;
 Прайсы
 */
 
+import com.sun.corba.se.impl.orbutil.CacheTable;
+
 import java.io.FileOutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -14,29 +16,33 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String fileName;
-        List<String> list = Files.readAllLines(Paths.get(fileName = sc.nextLine()), Charset.forName("windows-1251"));
-        int maxIndex = 0;
-        int index;
-
         try {
+            if (args[0].equals(null)) {}
+
+            Scanner sc = new Scanner(System.in);
+            String fileName;
+            List<String> list = Files.readAllLines(Paths.get(fileName = sc.nextLine()), Charset.forName("windows-1251"));
+
+            int index, maxIndex = 0;
             for (String str : list) {
                 if ((index = Integer.parseInt(str.substring(0, 8).trim())) > maxIndex) maxIndex = index;
             }
+
+            FileOutputStream stream = new FileOutputStream(fileName, true);
+
+            if (args[0].equals("-c")) {
+                String id = ConcatStr(++maxIndex + "", 8);
+                String productName = ConcatStr(args[1], 30);
+                String price = ConcatStr(args[2], 8);
+                String quantity = ConcatStr(args[3], 4);
+                stream.write(("\r\n" + id + productName + price + quantity).getBytes("windows-1251"));
+            }
+            stream.close();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Аргументы отсутствуют");
         }catch (NumberFormatException e){
             System.out.println("Не верный формат файла!");
         }
-
-        FileOutputStream stream = new FileOutputStream(fileName, true);
-        if (args[0].equals("-c")){
-            String id = ConcatStr(++maxIndex + "", 8);
-            String productName = ConcatStr(args[1], 30);
-            String price = ConcatStr(args[2], 8);
-            String quantity = ConcatStr(args[3], 4);
-            stream.write(("\r\n" + id + productName + price + quantity).getBytes("windows-1251"));
-        }
-        stream.close();
 
     }
 
