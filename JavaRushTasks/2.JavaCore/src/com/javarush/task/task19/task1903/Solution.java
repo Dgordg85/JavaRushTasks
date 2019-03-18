f9a248d00e047bc8234f1d1e36a0d68806b9ae68
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-18 10:18:59 +0300

    Адаптация нескольких интерфейсов

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task19.task1903;
+
+/* 
+Адаптация нескольких интерфейсов
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter {
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example: 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67
+    }
+}
