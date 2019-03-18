1a4b4221af050b4d7579421705176a11b073f28c
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-18 16:38:46 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task19.task1905;
 
+import com.sun.rowset.internal.Row;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,14 +10,99 @@ import java.util.Map;
 */
 
 public class Solution {
-    public static Map<String,String> countries = new HashMap<String,String>();
+    public static Map<String,String> countries = new HashMap<>();
 
-    public static void main(String[] args) {
-
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
     }
 
-    public static class DataAdapter {
+    public static void main(String[] args) {
+        Customer customer = new Test(null, null, "JavaRush Ltd.", "Ukraine");
+        Contact contact = new Test("Ivanov, Ivan", "+38(050)123-45-67", null, null);
+        RowItem item = new DataAdapter(customer, contact);
+
+        System.out.println(item.getCountryCode());
+        System.out.println(item.getCompany());
+        System.out.println(item.getContactFirstName());
+        System.out.println(item.getContactLastName());
+        System.out.println(item.getDialString());
+    }
+
+    public static class Test implements Customer, Contact{
+        private String name;
+        private String phoneNumber;
+        private String companyName;
+        private String countryName;
+
+        public Test(String name, String phoneNumber, String companyName, String countryName) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.companyName = companyName;
+            this.countryName = countryName;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return companyName;
+        }
+
+        @Override
+        public String getCountryName() {
+            return countryName;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+    }
+
+    public static class DataAdapter implements RowItem {
+        private Customer customer;
+        private Contact contact;
+
         public DataAdapter(Customer customer, Contact contact) {
+            this.contact = contact;
+            this.customer = customer;
+        }
+
+        @Override
+        public String getCountryCode() {
+            String str = null;
+            for (Map.Entry<String, String> entry : countries.entrySet()){
+                if (entry.getValue().equals(customer.getCountryName())){
+                    str = entry.getKey();
+                    break;
+                }
+            }
+            return str;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(" ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(" ")[0].replace(",", "");
+        }
+
+        @Override
+        public String getDialString() {
+            return "callto://" + contact.getPhoneNumber().replaceAll("[^+\\d]", "");
         }
     }
 
