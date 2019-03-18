335316323520bfd2c4759da0dcc2c13f6ca26fde
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-18 10:17:09 +0300

    Update AdapterFileOutputStream.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -4,12 +4,34 @@ package com.javarush.task.task19.task1902;
 Адаптер
 */
 
-public class AdapterFileOutputStream {
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AdapterFileOutputStream implements AmigoStringWriter {
+
+    private FileOutputStream fileOutputStream;
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream) {
+        this.fileOutputStream = fileOutputStream;
+    }
 
     public static void main(String[] args) {
 
     }
 
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
 
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.close();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        fileOutputStream.write(s.getBytes());
+    }
 }
 
