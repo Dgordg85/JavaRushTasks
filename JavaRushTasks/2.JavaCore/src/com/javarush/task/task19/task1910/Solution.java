3aac4c3211013ce4bd14f2c434d5042b136fc406
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-19 16:58:01 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -4,7 +4,22 @@ package com.javarush.task.task19.task1910;
 Пунктуация
 */
 
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()));
+        reader.close();
+
+        while(fileReader.ready()){
+            fileWriter.write(fileReader.readLine().replaceAll("[\\p{Punct}]*",""));
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
