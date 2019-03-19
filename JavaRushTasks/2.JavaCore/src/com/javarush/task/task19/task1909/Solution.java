1f430d51b5dbc7154da2dd97e917b61824002a98
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-19 16:24:27 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -4,7 +4,22 @@ package com.javarush.task.task19.task1909;
 Замена знаков
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()));
+        reader.close();
+
+        while(fileReader.ready()){
+            fileWriter.write(fileReader.readLine().replace(".", "!"));
+        }
+
+        fileReader.close();
+        fileWriter.close();
+
     }
 }
