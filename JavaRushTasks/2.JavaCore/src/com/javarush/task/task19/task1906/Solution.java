6433a70ce6da11e1f5a80eee57800cd050db4dfe
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-19 09:50:52 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
@@ -4,7 +4,26 @@ package com.javarush.task.task19.task1906;
 Четные символы
 */
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        FileReader readFile = new FileReader(reader.readLine());
+        FileWriter writFile = new FileWriter(reader.readLine());
+
+        int i = 0;
+        while(readFile.ready()){
+            int symbol = readFile.read();
+            if (i++ % 2 != 0){
+                writFile.write(symbol);
+            }
+        }
+
+        reader.close();
+        writFile.close();
+        readFile.close();
     }
 }
