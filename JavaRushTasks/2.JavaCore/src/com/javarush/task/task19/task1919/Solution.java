48304e02081c5452893cea562b8d07395b01d477
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-20 14:34:02 +0300

    Revert "Самый богатый"
    This reverts commit 9bc6f12e8a9b56c69142ad5729d2beb6e2688797.

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -4,36 +4,7 @@ package com.javarush.task.task19.task1919;
 Считаем зарплаты
 */
 
-import java.io.*;
-import java.util.*;
-
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        HashMap<String, Double> map = new HashMap<>();
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-
-        String str;
-        while ((str = reader.readLine()) != null){
-            String[] strSplit = str.split(" ");
-            String secondName = strSplit[0];
-            Double newValue = Double.parseDouble(strSplit[1]);
-
-            if (map.containsKey(secondName)){
-                Double oldValue = map.get(secondName);
-                map.replace(secondName, oldValue, oldValue + newValue);
-            } else {
-                map.put(secondName, newValue);
-            }
-        }
-        reader.close();
-
-        TreeSet<String> tree = new TreeSet<>();
-        for (Map.Entry entry : map.entrySet()){
-            tree.add(entry.getKey() + " " + entry.getValue());
-        }
-
-        for (String s : tree){
-            System.out.println(s);
-        }
+    public static void main(String[] args) {
     }
 }
