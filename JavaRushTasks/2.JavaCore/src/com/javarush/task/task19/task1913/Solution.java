d5961684ecfa2346d638bc8aeb715374c3f6de8f
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-20 10:19:14 +0300

    Выводим только цифры

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultOut = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+        System.setOut(defaultOut);
+
+        System.out.println(outputStream.toString().replaceAll("\\D", ""));
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
