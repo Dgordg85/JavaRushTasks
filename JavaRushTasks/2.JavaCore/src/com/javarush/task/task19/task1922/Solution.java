36734918492ee0806a0c96d9084f9b3b865cb7b8
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-20 16:03:22 +0300

    Ищем нужные строки

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<>();
+    private static final int COUNT_MAX = 2;
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader stream = new BufferedReader(new FileReader(reader.readLine()));
+        reader.close();
+
+        String str;
+
+        while((str = stream.readLine()) != null){
+            int count = 0;
+            String[] strArr = str.split(" ");
+
+            for(String s : strArr){
+                if (words.contains(s)) count++;
+                if (count > COUNT_MAX) break;
+            }
+            if (count == COUNT_MAX) System.out.println(str);
+        }
+        stream.close();
+    }
+}
