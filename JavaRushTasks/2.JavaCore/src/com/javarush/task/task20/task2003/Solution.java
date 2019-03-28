c4c2e9714e27a844b027271aecbb28835b062fa4
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-03-28 16:38:34 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* 
 Знакомство с properties
@@ -8,19 +11,28 @@ import java.io.*;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
-        //implement this method - реализуйте этот метод
+    public static void main(String[] args) throws Exception {
+
     }
 
+    public void fillInPropertiesMap() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream stream = new FileInputStream(reader.readLine());
+        load(stream);
+    }
+
+
     public void save(OutputStream outputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties fileProperties = new Properties();
+        fileProperties.putAll(properties);
+        fileProperties.store(outputStream, "Итоговый файл");
     }
 
     public void load(InputStream inputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties fileProperties = new Properties();
+        fileProperties.load(inputStream);
+        fileProperties.forEach((key, value) -> properties.put(key.toString(), value.toString()));
     }
 
-    public static void main(String[] args) {
 
-    }
 }
