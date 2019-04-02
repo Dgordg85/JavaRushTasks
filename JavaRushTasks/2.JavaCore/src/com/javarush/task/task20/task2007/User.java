e27e6d9337209c4441b558344b301dd2511a54a6
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-02 16:12:25 +0300

    Update User.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2007/User.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2007/User.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task20.task2007;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable {
     private String firstName;
     private String lastName;
     private Date birthDate;
