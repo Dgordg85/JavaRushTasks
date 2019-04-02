e1eb330d1fc38d79c08c85625efc0fddbb04c06e
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-02 16:12:21 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2006/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2006/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task20.task2006;
 
-import java.io.Serializable;
+import com.sun.corba.se.impl.io.InputStreamHook;
+import com.sun.corba.se.impl.io.OutputStreamHook;
+import com.sun.corba.se.pept.encoding.OutputObject;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +28,24 @@ public class Solution {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileOutputStream fileOutput = new FileOutputStream("C:/1/1.bin");
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
 
+        Human oleg = new Human();
+        Asset car = new Asset("Nissan Qashqai");
+        car.setPrice(1.55);
+        oleg.assets.add(car);
+
+        outputStream.writeObject(oleg);
+
+        outputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("C:/1/1.bin");
+        ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+        Object object = inputStream.readObject();
+        inputStream.close();
+
+        Human newHuman = (Human) object;
     }
 }
