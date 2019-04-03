d0b18d845261541f528b9f6cefea39e36f61300e
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-03 12:41:12 +0300

    Переопределение сериализации

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task20.task2015;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации
+*/
+public class Solution implements Runnable, Serializable {
+    transient private Thread runner;
+    private int speed;
+
+    public Solution(int speed) {
+        this.speed = speed;
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public void run() {
+        // do something here, doesn't matter what
+    }
+
+    /**
+     Переопределяем сериализацию.
+     Для этого необходимо объявить методы:
+     private void writeObject(ObjectOutputStream out) throws IOException
+     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+     Теперь сериализация/десериализация пойдет по нашему сценарию :)
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
