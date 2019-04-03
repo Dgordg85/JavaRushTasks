96b58122ff96afe3eeb3e193c4e074d5511224d2
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-03 12:29:31 +0300

    Update Solution.java

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -1,19 +1,30 @@
 package com.javarush.task.task20.task2014;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /* 
 Serializable Solution
 */
-public class Solution {
-    public static void main(String[] args) {
+public class Solution implements Serializable {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println(new Solution(4));
+
+        ObjectOutputStream streamOut = new ObjectOutputStream(new FileOutputStream("C:/1/2.bin"));
+        Solution savedObject = new Solution(25);
+        streamOut.writeObject(savedObject);
+        streamOut.close();
+
+        ObjectInputStream streamIn = new ObjectInputStream(new FileInputStream("C:/1/2.bin"));
+        Solution openObject = (Solution) streamIn.readObject();
+        System.out.println(openObject);
+        streamIn.close();
     }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    transient private final String pattern = "dd MMMM yyyy, EEEE";
+    transient private Date currentDate;
+    transient private int temperature;
     String string;
 
     public Solution(int temperature) {
