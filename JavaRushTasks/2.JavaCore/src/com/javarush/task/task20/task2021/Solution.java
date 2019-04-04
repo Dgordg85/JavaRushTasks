c2d8af499b860b4383166a38eab27682b1c3235a
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-04 17:24:04 +0300

    Сериализация под запретом

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        private void writeObject(ObjectOutputStream stream) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+
+        private void readObject(ObjectInputStream stream) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
