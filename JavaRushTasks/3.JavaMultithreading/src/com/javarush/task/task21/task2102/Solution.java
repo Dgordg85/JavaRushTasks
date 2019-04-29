e2b2014c1016ac5782645f3f37a402bd179fcc0c
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-04-29 14:56:54 +0300

    Сравниваем модификаторы

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2102/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task21.task2102;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/* 
+Сравниваем модификаторы
+*/
+public class Solution {
+    public static void main(String[] args) {
+        int classModifiers = Solution.class.getModifiers();
+        System.out.println(isModifierSet(classModifiers, Modifier.PUBLIC));   //true
+        System.out.println(isModifierSet(classModifiers, Modifier.STATIC));   //false
+
+        int methodModifiers = getMainMethod().getModifiers();
+        System.out.println(isModifierSet(methodModifiers, Modifier.STATIC));      //true
+    }
+
+    public static boolean isModifierSet(int allModifiers, int specificModifier) {
+        allModifiers &= specificModifier;
+        switch (specificModifier){
+            case Modifier.PUBLIC:
+                if (Modifier.isPublic(allModifiers)) return true;
+                break;
+            case Modifier.STATIC:
+                if (Modifier.isStatic(allModifiers)) return true;
+                break;
+        }
+        return false;
+    }
+
+    private static Method getMainMethod() {
+        Method[] methods = Solution.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equalsIgnoreCase("main")) return method;
+        }
+
+        return null;
+    }
+}
