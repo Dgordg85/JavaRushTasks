197696b9b99c9d52eb2af3757e7a52e0e569aade
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-05-06 16:59:39 +0300

    Ипподром 2

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,7 +1,15 @@
 package com.javarush.task.task21.task2113;
 
-public class Hippodrome {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Hippodrome {
+    private List<Horse> horses;
+
+    public static void main(String[] args) {
+        
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
     }
 }
