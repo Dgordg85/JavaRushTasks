ed5716fedc7cea4ae34ffde45e4fb4ed53f72cfc
Dgordg85 <Dgordg85@users.noreply.github.com>
2019-05-07 11:34:08 +0300

    Ипподром 3

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Hippodrome {
     private List<Horse> horses;
 
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
     public static void main(String[] args) {
         
     }
